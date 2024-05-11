3a636a39c94ff102206a81e97e3d49d2c636e4b2
EpicOfficer <9379778+EpicOfficer@users.noreply.github.com>
2024-05-11 19:22:02 +0100

    Whole lotta branding

---
+++ config/files/usr/etc/profile.d/bazzite-neofetch.sh
@@ -0,0 +1,4 @@
+alias neofetch='/usr/bin/fastfetch -c /usr/share/quantix-os/fastfetch.jsonc'
+alias neowofetch='/usr/bin/fastfetch -c /usr/share/quantix-os/fastfetch.jsonc'
+alias hyfetch='/usr/bin/fastfetch -c /usr/share/quantix-os/fastfetch.jsonc'
+alias fastfetch='/usr/bin/fastfetch -c /usr/share/quantix-os/fastfetch.jsonc'
