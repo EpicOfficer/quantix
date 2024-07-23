8d1c74ce2097bfd11ad9501dffb63df788881230
EpicOfficer <9379778+EpicOfficer@users.noreply.github.com>
2024-07-23 20:31:10 +0100

    Test migration to bluebuild

--- files/scripts/qtx-info.sh
+++ files/scripts/qtx-info.sh
@@ -1,6 +1,9 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 set -oue pipefail
+
+sed -i '/^PRETTY_NAME/s/Bazzite/Quantix/' /usr/lib/os-release
+sed -i 's/plasma.svgz/quantix_logo.svgz/g' /usr/share/plasma/look-and-feel/org.kde.breeze.desktop/contents/splash/Splash.qml
 
 cat > /usr/share/quantix-os/image-info.json <<EOF
 {
