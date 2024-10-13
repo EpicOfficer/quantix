d1b660cde2d34c6d8f7aef2efa96c3b040529979
EpicOfficer <9379778+EpicOfficer@users.noreply.github.com>
2024-10-13 15:24:28 +0100

    Move SDDM back into repo as the github version is broken

--- files/scripts/install-dracula.sh
+++ files/scripts/install-dracula.sh
@@ -12,12 +12,18 @@ mkdir -p /usr/share/sddm/themes
 mkdir -p /usr/share/color-schemes
 mkdir -p /usr/share/themes
 mkdir -p /usr/share/icons
+mkdir -p /usr/etc/skel/Kvantum
 
+cp -r gtk/assets /usr/etc/skel/.config/
+cp gtk/gtk-4.0/gtk.css /usr/etc/xdg/gtk-4.0/
+cp gtk/gtk-4.0/gtk-dark.css /usr/etc/xdg/gtk-4.0/
 mv gtk/kde/plasma/desktoptheme/* /usr/share/plasma/desktoptheme/ 
 mv gtk/kde/plasma/look-and-feel/Plasma6/Dracula /usr/share/plasma/look-and-feel/
-mv gtk/kde/sddm/Dracula /usr/share/sddm/themes/
 mv gtk/kde/cursors/Dracula-cursors /usr/share/icons/
 mv gtk/kde/aurorae/* /usr/share/aurorae/themes/
 mv gtk/kde/color-schemes/* /usr/share/color-schemes/
+mv gtk/kde/kvantum/* /usr/etc/skel/Kvantum
+
+rm -R gtk/src gtk/unity gtk/xfwm4 gtk/kde gtk/cinnamon gtk/Art gtk/*.json gtk/*.md gtk/*.js gtk/LICENSE
 
 mv gtk /usr/share/themes/Dracula
